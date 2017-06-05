abba4d7ede833aa7dfcc5cab0f766d67f55d236f
mwarszczynski <m.warszczynski@gmail.com>
2017-06-05 11:22:28 +0200

    I`m going to create advanced lotto game with gui

---
+++ Big Lotto game/scripts.js
@@ -0,0 +1,19 @@
+var randoming = 0;
+
+function bigLotto() {
+	var numbers = [1,2,3,4,5,6,7,
+						8,9,10,11,12,13,14,
+						15,16,17,18,19,20,21,
+						22,23,24,25,26,27,28,
+						29,30,31,32,33,34,35,
+						36,37,38,39,40,41,42,
+						43,44,45,46,47,48,49];
+	
+	while(randoming < 6) {
+		var rand = Math.floor(Math.random() * numbers.length);
+		randoming++;
+		console.log('The drawn numbers are ' + rand);
+	}
+}
+
+bigLotto();

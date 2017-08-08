f20ad673c83db8e40dc7a296e7e4c19a6c9d4f04
mwarszczynski <m.warszczynski@gmail.com>
2017-08-08 13:03:04 +0200

    learning js with js30

---
+++ js-30/1-JS-Drum-Kit/scripts.js
@@ -0,0 +1,11 @@
+window.addEventListener('keydown', function(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return; //stop func
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add('playing');
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach()

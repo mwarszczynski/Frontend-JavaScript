fdbb1e5b36b6939d631ac8112595639863c64022
mwarszczynski <mwarszczynski@users.noreply.github.com>
2017-08-24 12:16:01 +0200

    to-do app ANGULAR 4
    Simply to do app in ANGULAR 4.
    
    * I`m trying to implement mongo db and express with Ag-4.
    * I would like to improve this project and add ability to create,put,update,delete (crud) data/task.

---
+++ to-do/controllers/todoListController.js
@@ -0,0 +1,56 @@
+// In this controller, we would be writing five(5)
+// different functions namely:
+// list_all_tasks, create_a_task, read_a_task,
+// update_a_task, delete_a_task
+
+'use strict';
+
+var mongoose = require('mongoose'),
+  Task = mongoose.model('Tasks');
+
+exports.list_all_tasks = function(req, res) {
+  Task.find({}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.create_a_task = function(req, res) {
+  var new_task = new Task(req.body);
+  new_task.save(function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+exports.read_a_task = function(req, res) {
+  Task.findById(req.params.taskId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.update_a_task = function(req, res) {
+  Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(task);
+  });
+};
+
+
+exports.delete_a_task = function(req, res) {
+
+
+  Task.remove({
+    _id: req.params.taskId
+  }, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Task successfully deleted' });
+  });
+};
